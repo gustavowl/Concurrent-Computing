3dc4889e13a0d8d2b6d6ce47bf9dfe419b0dfc83
Gustavo <gustavoalvesbezerra@gmail.com>
2017-04-17 22:24:26 -0300

    Tests implementation of matrix multiplication with threads

--- MatrixMultiplication/threaded_matrix_mult.py
+++ MatrixMultiplication/threaded_matrix_mult.py
@@ -1,3 +1,21 @@
+import threading
+
+#this algorithm will compute, at least, one row of the result matrix.
+#matrix_a * matrix_b = matrix
+#row_start <= row_end
+#row_start is the index of the first row the result matrix to be computed
+#row_end is the last row to be computed
+def multiply_matrix(matrix_a, matrix_b, row_start, row_end, matrix):
+	for i in range(row_start, row_end + 1): #supposes that matrix has at least 1 row
+		result2 = []
+		for j in range(len(matrixB[0])):
+			val = 0
+			for k in range(len(matrixB)):
+				val += matrixA[i][k] * matrixB[k][j]
+			result2.append(val)
+		matrix[i] = result2
+
+
 #generates matrix from file
 with open("matrixA.txt") as file:
 	content = file.readlines() #saves each line to content.
@@ -22,16 +40,15 @@ for i in range(len(content)):
 	matrixB.append([eval(n) for n in content[i].split()]) #also converts to float list
 #ends matrix generation
 
-#multiplies matrices. matrixA x matrixB
-result = [] #declares result matrix
-#number of columns and rows is not verified
-for i in range(len(matrixA)): #supposes that matrix has at least 1 row
-	result2 = []
-	for j in range(len(matrixB[0])):
-		val = 0
-		for k in range(len(matrixB)):
-			val += matrixA[i][k] * matrixB[k][j]
-		result2.append(val)
-	result.append(result2)
+result = [[0] * len(matrixA)] * len(matrixB[0])
 
+
+t1 = threading.Thread(target=multiply_matrix, args=(
+	matrixA, matrixB, 0, 1, result))
+t2 = threading.Thread(target=multiply_matrix, args=(
+	matrixA, matrixB, 2, 3, result))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
 print(result)
