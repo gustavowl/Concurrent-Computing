38aaeec27a710bedd405fc401675bbf8733e4176
Gustavo <gustavoalvesbezerra@gmail.com>
2017-08-19 21:27:05 -0300

    Implements script for reading sequential data

--- MatrixMultiplication/organize_data.sh
+++ MatrixMultiplication/organize_data.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 if [ $# = 2 ]; then
-	echo right number of args
-	#Implement loop for reading
+	first=true
+	echo -ne '(' > $2
+	while read -r line; do
+		if [ `expr match "$line" 'Time to multiply matrices: '` -gt 0 ]; then
+			temp=${line:27}
+			temp=${temp:0:`expr index "$temp" ' '`}
+			if [ $first = false ]; then
+				echo -ne ', '$temp >> $2
+			else
+				first=false
+				echo -ne $temp >> $2
+			fi
+		fi
+
+	done < $1
+	echo ')' >> $2
 else
 	echo 'Wrong number of arguments. Expecting 2 arguments:'
 	echo -e '1 - Benchmark file\n2 - Output file'
